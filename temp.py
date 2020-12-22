e9d6eb8d2a30cd4617383f3330d54a3bb76ab56a
typass <typass21@gmail.com>
2020-12-22 16:08:53 +0900

    rm

--- temp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-fvs_path = './fvs_clustering/'
-# normal
-fvs = np.load(fvs_path + 'fvs_DAGM2007.npy')
-# label = np.load(fvs_path + 'label_DAGM2007.npy')
-print(fvs.shape)
-# print(label.shape)
-
-
